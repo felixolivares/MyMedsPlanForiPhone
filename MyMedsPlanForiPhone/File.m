45128f7bf48abc07c50e29730b10bbb3c40ab8ea
Felix Olivares <felixolivares@iT-MacBook-Pro-2596.local>
2015-08-05 14:13:37 -0600

    Add MagicalRecord submodule

---
+++ MyMedsPlanForiPhone/File.m
@@ -0,0 +1,9 @@
+//
+//  File.m
+//  MyMedsPlanForiPhone
+//
+//  Created by Felix Olivares on 7/9/15.
+//  Copyright © 2015 Felix Olivares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ MyMedsPlanForiPhone/MyMedsPlanForiPhone-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MyMedsPlanForiPhone-Bridging-Header.h
+//  MyMedsPlanForiPhone
+//
+//  Created by Felix Olivares on 7/9/15.
+//  Copyright © 2015 Felix Olivares. All rights reserved.
+//
+
+#ifndef MyMedsPlanForiPhone_Bridging_Header_h
+#define MyMedsPlanForiPhone_Bridging_Header_h
+
+
+#endif /* MyMedsPlanForiPhone_Bridging_Header_h */
